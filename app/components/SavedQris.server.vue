262467e93019e8e4c7d92ffc30b643dfc8c80547
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-08-25 14:05:43 +0700

    fix: hydration missmatch

--- app/components/SavedQris.server.vue
+++ app/components/SavedQris.server.vue
@@ -1,29 +1,19 @@
-<script setup lang="ts">
-const [DefineSkeletonTemplate, ReuseSkeletonTemplate] =
-  createReusableTemplate();
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <DefineSkeletonTemplate>
-    <div class="ring ring-default p-4 rounded-md">
-      <div class="flex flex-row space-x-3">
-        <USkeleton class="size-18 lg:size-24 rounded-lg flex-none" />
-        <div class="flex flex-col w-full">
-          <USkeleton class="h-6" />
-          <USkeleton class="h-5 mt-1" />
-
-          <USkeleton class="h-9 w-24 mt-3" />
-        </div>
-      </div>
-    </div>
-  </DefineSkeletonTemplate>
-
   <CardSection title="Riwayat">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-      <ReuseSkeletonTemplate />
-      <ReuseSkeletonTemplate />
-      <ReuseSkeletonTemplate />
-      <ReuseSkeletonTemplate />
+      <div v-for=" in 4" class="ring ring-default p-4 rounded-md">
+        <div class="flex flex-row space-x-3">
+          <USkeleton class="size-18 lg:size-24 rounded-lg flex-none" />
+          <div class="flex flex-col w-full">
+            <USkeleton class="h-6" />
+            <USkeleton class="h-5 mt-1" />
+
+            <USkeleton class="h-9 w-24 mt-3" />
+          </div>
+        </div>
+      </div>
     </div>
   </CardSection>
 </template>
